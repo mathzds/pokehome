66aeea5da1be40b9162c1cb63e3e4297d4e55c0b
Pedro Machado <pedrofilhodev@gmail.com>
2025-10-01 22:30:16 -0300

    Remove pasta do versionamento

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import Server from "./infrastructure/http/server.js";
-
-async function main() {
-  const server = Server.getInstance();
-  await server.listen();
-}
-
-main();
--- server/src/infrastructure/http/routes/router.ts
+++
@@ -1,7 +0,0 @@
-import type { Hono } from "hono";
-
-export default async function Router(app: Hono) {
-  app.get("/", (c) => {
-    return c.json({ message: "Hello World!" });
-  });
-}
